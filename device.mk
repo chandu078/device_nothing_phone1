4ff0800823ec3dda4dc7ed502c5455e3ba163686
Jiyong Park <jiyong@google.com>
2019-05-18 21:37:03 +0900

    phone1: Declare support for updatable APEXes
    Change-Id: I4338b650eade5c9fdc2c4650fae103e1a52f46bb
    Signed-off-by: chandu078 <chandudyavanapelli03@gmail.com>

--- device.mk
+++ device.mk
@@ -4,6 +4,9 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# APEX
+$(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
+
 # A/B
 $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
 
