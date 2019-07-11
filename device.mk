91dce3912b57bf28236e3228b8edd4622a2ba15b
LuK1337 <priv.luk@gmail.com>
2019-07-11 15:32:52 +0200

    phone1: init: Set SSR restart level to 'RELATED'
    Change-Id: I32fc08c9ad36b9c8ebfc019fc87a19eece18f0b4
    Signed-off-by: chandu078 <chandudyavanapelli03@gmail.com>

--- device.mk
+++ device.mk
@@ -240,6 +240,7 @@ PRODUCT_PACKAGES += \
     fstab.default \
     fstab.default.vendor_ramdisk \
     init.class_main.sh \
+    init.nt.rc \
     init.qcom.early_boot.sh \
     init.qcom.rc \
     init.qcom.recovery.rc \
