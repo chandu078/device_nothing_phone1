527e1262bbe206b06cffe458c641ec721ddfca88
Lokesh Gidra <lokeshgidra@google.com>
2019-12-06 14:00:16 -0800

    phone1: Inherit the Dalvik heap configuration for a 6GB device
     * Nothing phone (1) has multiple variants with varying RAM capacities
       but it's not worth making custom values for each variant,
       can end up doing more harm than good.
    
     * For now lets just use the verified ones from AOSP for the
       highest capacity variant they have.
    
    Change-Id: Ib6b1d7ad933fe3867dc6253e229a3c6470135e75
    Signed-off-by: chandu078 <chandudyavanapelli03@gmail.com>

--- device.mk
+++ device.mk
@@ -146,6 +146,9 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.camera.full.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.full.xml \
     frameworks/native/data/etc/android.hardware.camera.raw.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.raw.xml
 
+# Dalvik
+$(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
+
 # Display
 PRODUCT_PACKAGES += \
     android.hardware.graphics.mapper@3.0-impl-qti-display \
