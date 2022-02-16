e876287b6ad89dc6550662259370a1e50aa05bba
Cosmin Tanislav <demonsingur@gmail.com>
2022-02-16 22:14:58 +0200

    phone1: Add dummy sensors sub hal
    Change-Id: Ie20ed775344990de3e16e2eefba1754d831027fb
    Signed-off-by: aswin7469 <aswinas@pixysos.com>
    Signed-off-by: chandu078 <chandudyavanapelli03@gmail.com>

--- device.mk
+++ device.mk
@@ -362,7 +362,8 @@ PRODUCT_PACKAGES += \
 # Sensors
 PRODUCT_PACKAGES += \
     android.hardware.sensors@2.0-service.multihal \
-    libsensorndkbridge
+    libsensorndkbridge \
+    sensors.nt
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/sensors/hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/hals.conf
