91bc9cc7a3f6b05a9dd6499d077fe13ec66641f4
chandu078 <chandudyavanapelli03@gmail.com>
2022-12-01 11:59:33 +0530

    phone1: Import recovery kernel modules list from NT 1.1.6
    Signed-off-by: chandu078 <chandudyavanapelli03@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -77,6 +77,8 @@ TARGET_KERNEL_CONFIG := vendor/lahaina-qgki_defconfig
 
 # Kernel modules
 BOARD_VENDOR_KERNEL_MODULES_LOAD := $(strip $(shell cat $(DEVICE_PATH)/modules.load))
+BOARD_VENDOR_RAMDISK_RECOVERY_KERNEL_MODULES_LOAD := $(strip $(shell cat $(DEVICE_PATH)/modules.load.recovery))
+BOOT_KERNEL_MODULES := $(BOARD_VENDOR_RAMDISK_RECOVERY_KERNEL_MODULES_LOAD)
 
 # Metadata
 BOARD_USES_METADATA_PARTITION := true
