c818688ea7a9ada9e9c0f7939931e43b6f1d5d66
chandu078 <chandudyavanapelli03@gmail.com>
2022-12-02 15:49:00 +0530

    phone1: Include QCOM SEPolicy
    Signed-off-by: chandu078 <chandudyavanapelli03@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -163,6 +163,9 @@ TARGET_USERIMAGES_USE_F2FS := true
 # RIL
 ENABLE_VENDOR_RIL_SERVICE := true
 
+# SELinux
+include device/qcom/sepolicy_vndr/SEPolicy.mk
+
 # Security
 BOOT_SECURITY_PATCH := 2022-09-01
 VENDOR_SECURITY_PATCH := $(BOOT_SECURITY_PATCH)
