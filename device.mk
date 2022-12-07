7135f58c73cffa7301c641796a53ac1fbf5c2dfa
chandu078 <chandudyavanapelli03@gmail.com>
2022-12-07 05:30:46 +0000

    phone1: Add NT camera support  * Camera repository: https://gitlab.com/ThankYouMario/proprietary_vendor_nothing_camera.
    Signed-off-by: chandu078 <chandudyavanapelli03@gmail.com>

--- device.mk
+++ device.mk
@@ -130,6 +130,8 @@ PRODUCT_PACKAGES += \
     android.hardware.boot@1.2-service
 
 # Camera
+$(call inherit-product, vendor/nothing/camera/nothing-camera.mk)
+
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service_64 \
